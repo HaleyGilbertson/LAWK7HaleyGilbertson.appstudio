2ef07b2ed340a053407018de230764d586587657
HaleyGilbertson <hag79621@creighton.edu>
2021-10-20 14:40:53 -0500

    new customerSelect

---
+++ forms/customerAdd/customerAdd.js
@@ -0,0 +1,10 @@
+/*
+btnSubmit.onclick = function() {
+    name = inptName.value
+    address = inptAddress.value
+    city = inptCity.value
+    state = inptState.value
+    zipcode = inptZipcode.value
+    query = "INSERT INTO customer (name,street,city,state,zipcode) VALUES ('" + name + "', '" + address + "', '" + city + "', '" + state + "', '" + zip + "')"
+    alert(query)
+    */
